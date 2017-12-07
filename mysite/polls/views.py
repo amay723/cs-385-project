499db7952fb096fb381efa9155f370d3087063b7
amay723 <31995344+amay723@users.noreply.github.com>
2017-12-06 19:22:19 -0800

    Add files via upload

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,6 +8,8 @@ from django.shortcuts import render
 #from django.template import loader
 
 number = 1
+player = 'X'
+grid = []
 
 def index(request):
     #template = loader.get_template('polls/index.html')
@@ -15,14 +17,22 @@ def index(request):
     global number
     number = 2
 
+    global grid
     grid = [["." for i in range(3)] for j in range(3)]
 
-    i = [0, 1, 2]
-    j = [0, 1, 2]
+    inner = [0, 1, 2]
+    outer = [0, 1, 2]
+
+    newgrid = []
+    for i in range(0,3):
+        for j in range(0,3):
+            newgrid.append(grid[i][j])
+
     context = {
-        'game_board': grid,
-        'outer': i,
-        'inner': j
+        'game_board': newgrid,
+        'inner': inner,
+        'outer': outer,
+        'player': player
     }
 
     return render(request, 'polls/index.html', context)
@@ -38,3 +48,124 @@ def detail(request, sector_id):
     
     return render(request, 'polls/index.html', context)
     
+
+def done(grid):
+
+    if grid[0][1] == '0' and grid[0][0] == '0' and grid[0][2] == '0':
+        return True
+    elif grid[0][1] == 'X' and grid[0][0] == 'X' and grid[0][2] == 'X':
+        return True
+    # Top Row win cond
+
+    elif grid[1][0] == '0' and grid[1][1] == '0' and grid[1][2] == '0':
+        return True
+    elif grid[1][0] == 'X' and grid[1][1] == 'X' and grid[1][2] == 'X':
+        return True
+    # Mid Row win cond
+
+    elif grid[2][0] == '0' and grid[2][1] == '0' and grid[2][2] == '0':
+        return True
+    elif grid[2][0] == 'X' and grid[2][1] == 'X' and grid[2][2] == 'X':
+        return True
+    # Bottom Row win cond
+
+    elif grid[0][1] == '0' and grid [1][1] == '0' and grid[2][1] == '0':
+        return True
+    elif grid[0][1] == 'X' and grid [1][1] == 'X' and grid[2][1] == 'X':
+        return True
+    # Mid down win cond
+
+    elif grid[0][0] == '0' and grid[1][1] == '0' and grid[2][2] == '0':
+        return True
+    elif grid[0][0] == 'X' and grid[1][1] == 'X' and grid[2][2] == 'X':
+        return True
+    # Diagonal Right
+
+    elif grid[2][0] == '0' and grid[1][1] == '0' and grid[0][2] == '0':
+        return True
+    elif grid[2][0] == 'X' and grid[1][1] == 'X' and grid[0][2] == 'X':
+        return True
+    # Diagonal Left
+
+    elif grid[0][2] == '0' and grid[1][2] == '0' and grid[2][2] == '0':
+        return True
+    elif grid[0][2] == 'X' and grid[1][2] == 'X' and grid[2][2] == 'X':
+        return True
+    # Left Side Vertical
+
+    elif grid[0][0] == '0' and grid[1][0] == '0' and grid[2][0] == '0':
+        return True
+    elif grid[0][0] == 'X' and grid[1][0] == 'X' and grid[2][0] == 'X':
+        return True
+    # Right Side Vertical
+
+    else:
+        return False
+
+
+def move(request, sector_id):
+
+    sector = sector_id
+
+    global grid
+    global player
+
+    if player == "X":
+        if sector == "1":
+            grid[0][0] = 'X'
+        elif sector == "2":
+            grid[0][1] = 'X'
+        elif sector == "3":
+            grid[0][2] = 'X'
+        elif sector == "4":
+            grid[1][0] = 'X'
+        elif sector == "5":
+            grid[1][1] = 'X'
+        elif sector == "6":
+            grid[1][2] = 'X'
+        elif sector == "7":
+            grid[2][0] = 'X'
+        elif sector == "8":
+            grid[2][1] = 'X'
+        elif sector == "9":
+            grid[2][2] = "X"
+        player = "O"
+    elif player == "O":
+        if sector == "1":
+            grid[0][0] = 'O'
+        elif sector == "2":
+            grid[0][1] = 'O'
+        elif sector == "3":
+            grid[0][2] = 'O'
+        elif sector == "4":
+            grid[1][0] = 'O'
+        elif sector == "5":
+            grid[1][1] = 'O'
+        elif sector == "6":
+            grid[1][2] = 'O'
+        elif sector == "7":
+            grid[2][0] = 'O'
+        elif sector == "8":
+            grid[2][1] = 'O'
+        elif sector == "9":
+            grid[2][2] = "O"
+        player = "X"
+            
+    
+    inner = [0, 1, 2]
+    outer = [0, 1, 2]
+
+    newgrid = []
+    for i in range(0,3):
+        for j in range(0,3):
+            newgrid.append(grid[i][j])
+
+    context = {
+        'game_board': newgrid,
+        'inner': inner,
+        'outer': outer,
+        'lastSector': sector,
+        'player': player
+    }
+
+    return render(request, 'polls/index.html', context)
